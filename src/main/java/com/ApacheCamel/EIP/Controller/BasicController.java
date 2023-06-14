69ff7afda22f7fa0089ac7429e0f5b1c369a1021
MahmoudBadran <mahmoud.bdran20@gmail.com>
2023-06-14 12:30:00 +0300

    unMarshal json to string

--- src/main/java/com/ApacheCamel/EIP/Controller/BasicController.java
+++ src/main/java/com/ApacheCamel/EIP/Controller/BasicController.java
@@ -1,14 +1,35 @@
 package com.ApacheCamel.EIP.Controller;
 
+import com.ApacheCamel.EIP.Entity.Student;
+import com.ApacheCamel.EIP.Service.StudentService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BasicController {
 
-    @GetMapping("JustAString")
+
+    @Autowired
+    StudentService studentService;
+    @GetMapping("/JustAString")
     public ResponseEntity<String> sayHello(){
         return ResponseEntity.ok("this is from a rest service");
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<List<Student>> getStudents(){
+        return ResponseEntity.ok(studentService.initStudentModel());
+    }
+    @GetMapping("/students/1")
+    public ResponseEntity<Student> getStudentById(){
+        Student student =new Student(1,"mahmoud","grade 1");
+
+        return ResponseEntity.ok(student);
     }
 }
--- src/main/java/com/ApacheCamel/EIP/Routes/RestAPI/RestAPIRoute.java
+++ src/main/java/com/ApacheCamel/EIP/Routes/RestAPI/RestAPIRoute.java
@@ -1,6 +1,8 @@
 package com.ApacheCamel.EIP.Routes.RestAPI;
 
+import com.ApacheCamel.EIP.Entity.Student;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,9 +12,15 @@ public class RestAPIRoute extends RouteBuilder {
         from("timer:delayTimer?repeatCount=1")
                 .to("direct:start");
 
+//        from("direct:start")
+//                .to("http://localhost:9090/JustAString")
+//                .convertBodyTo(String.class)
+//                .to("file:D:/dest/RestApi?fileName=restResponse.txt")
+//                .to("direct:getStudents");
+
         from("direct:start")
-                .to("http://localhost:9090/JustAString")
+                .to("http://localhost:9090/students")
                 .convertBodyTo(String.class)
-                .to("file:D:/dest/RestApi?fileName=restResponse.txt");
+                .to("file:D:/dest/RestApi?fileName=JsonStudent.txt");
     }
 }
